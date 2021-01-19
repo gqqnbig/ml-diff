5d9548f186be7c3773b3aaa9497fe579378496a4
QQ喵 <gqqnb2005@gmail.com>
2021-01-19 21:28:43 +0800

    Add test examples

---
+++ DiffSyntax/test/FilterExample-0-before.java
@@ -0,0 +1,7 @@
+ 	 *	image is a rectangular matrix, neighberhoodSize > 0
+ 	 *post: return a smoothed version of image
+ 	 */
+	public Color[][] smooth(Color[][] image, int neighberhoodSize)
+ 	{	//check precondition
+ 		assert image != null && image.length > 1 && image[0].length > 1
+ 				&& ( neighberhoodSize > 0 ) && rectangularMatrix( image )
