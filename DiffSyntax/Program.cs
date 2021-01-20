8e170f374e5f91f3c609481890e30cb925e664d6
QQ喵 <gqqnb2005@gmail.com>
2021-01-20 12:55:53 +0800

    can find longest match

--- DiffSyntax/Program.cs
+++ DiffSyntax/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Diagnostics;
+using System.Reflection;
 using Antlr4.Runtime;
 using Antlr4.Runtime.Misc;
 
@@ -10,92 +12,88 @@ namespace DiffSyntax
 
 		static void Main(string[] args)
 		{
-			string content;
+			string input;
 			using (StreamReader sr = new StreamReader(@"D:\renaming\neural network\DiffSyntax\test\FilterExample-0-before.java"))
 			{
-				content = sr.ReadToEnd();
+				input = sr.ReadToEnd();
 			}
 
-			var stream = CharStreams.fromString(content);
+			var stream = CharStreams.fromString(input);
 			ITokenSource lexer = new JavaLexer(stream);
+
 
 			ITokenStream tokens = new CommonTokenStream(lexer);
 			var parser = new JavaParser(tokens);
 			parser.ErrorHandler = new BailErrorStrategy();
+			parser.RemoveErrorListeners();
 
-			foreach(string ruleName in parser.RuleIndexMap.Keys)
+
+			Type typeJavaParser = typeof(JavaParser);
+
+			int stopIndex = 0;
+			string longestMatchRule;
+			foreach (string ruleName in parser.RuleIndexMap.Keys)
 			{
-				Console.WriteLine($"Try rule {ruleName}...");
-				int ruleIndex = parser.RuleIndexMap[ruleName];
+				tokens.Seek(0);
+				//parser = new JavaParser(tokens);
+				//parser.ErrorHandler = new BailErrorStrategy();
+				//parser.RemoveErrorListeners();
+				Console.Write($"Try rule {ruleName}...");
+
 
 				try
 				{
-					var context = parser.GetInvokingContext(ruleIndex);
-					//var tree = parser.classBodyDeclaration();
+					var m = typeJavaParser.GetMethod(ruleName, BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+					Debug.Assert(m != null);
+					ParserRuleContext context = (ParserRuleContext)m.Invoke(parser, new object[0]);
 
-					Console.WriteLine($"{ruleName} produced a full match.");
+					if (context == null)
+						Console.WriteLine($"{ruleName} is not a match.");
+					else
+					{
+						//var tree = parser.classBodyDeclaration();
+
+						Console.WriteLine($"{ruleName} produced a full match, stoped at {context.Stop.StopIndex}.");
+						if (context.Stop.StopIndex > stopIndex)
+						{
+							stopIndex = context.Stop.StopIndex;
+							longestMatchRule = ruleName;
+						}
+					}
+				}
+				catch (TargetInvocationException e)
+				{
+					if (e.InnerException is ParseCanceledException)
+					{
+						RecognitionException recongnitionException = (RecognitionException)e.InnerException.InnerException;
+						var tree = recongnitionException.Context;
+
+						if (recongnitionException.OffendingToken.StartIndex == input.Length)
+						{
+							Console.WriteLine($"{ruleName} stoped at the end of input. The input is an incomplete syntax unit.");
+
+							Debug.Assert(recongnitionException.OffendingToken.StartIndex >= stopIndex);
+							longestMatchRule = ruleName;
+							break;
+						}
+						else {
+
+							Console.WriteLine($" can match up to {tree.SourceInterval.b}, and IsEmpty={tree.IsEmpty}.");
+							if (tree.SourceInterval.b > stopIndex)
+							{
+								stopIndex = tree.SourceInterval.b;
+								longestMatchRule = ruleName;
+							}
+						}
+					}
 				}
 				catch (ParseCanceledException e)
 				{
-					var tree = ((RecognitionException)e.InnerException).Context;
-					if (((RecognitionException)e.InnerException).OffendingToken.StartIndex == content.Length)
-					{
-						Console.WriteLine($"{ruleName} stoped at the end of input. The input is an incomplete syntax unit.");
-						break;
-					}
-					else
-					{
 
-						//The chosen context is incorrect. Let's try the next one.
-
-					}
 				}
 			}
 
-//			var identifierCollector = new IdentifierCollector();
-//			Action<JavaParser>[] matchSequences = {
-//p => identifierCollector.Visit(p.compilationUnit()),
-//p => identifierCollector.Visit(p.typeDeclaration()),
-//p => identifierCollector.Visit(p.classDeclaration()),
-//p => identifierCollector.Visit(p.enumDeclaration()),
-//p => identifierCollector.Visit(p.enumConstants()),
-//p => identifierCollector.Visit(p.enumConstant()),
-//p => identifierCollector.Visit(p.enumBodyDeclarations()),
-//p => identifierCollector.Visit(p.interfaceDeclaration()),
-//p => identifierCollector.Visit(p.classBody()),
-//p => identifierCollector.Visit(p.interfaceBody()),
-//p => identifierCollector.Visit(p.classBodyDeclaration()),
-//// p => identifierCollector.Visit(p.memberDeclaration()), // memberDeclaration is an alias of a group of rules.
-//p => identifierCollector.Visit(p.methodDeclaration()),
-//p => identifierCollector.Visit(p.methodBody()),
-//p => identifierCollector.Visit(p.genericMethodDeclaration()),
-//p => identifierCollector.Visit(p.genericConstructorDeclaration()),
-//p => identifierCollector.Visit(p.constructorDeclaration()),
-//p => identifierCollector.Visit(p.interfaceBodyDeclaration()),
-//// p => identifierCollector.Visit(p.interfaceMemberDeclaration()), // interfaceMemberDeclaration is an alias of a group of rules.
-//p => identifierCollector.Visit(p.constDeclaration()),
-//p => identifierCollector.Visit(p.constructorDeclaration()),
-//p => identifierCollector.Visit(p.constructorDeclaration()),
-//p => identifierCollector.Visit(p.constructorDeclaration()),
-//p => identifierCollector.Visit(p.constructorDeclaration()),
-//p => identifierCollector.Visit(p.constructorDeclaration()),
-//p => identifierCollector.Visit(p.constructorDeclaration()),
-//p => identifierCollector.Visit(p.constructorDeclaration()),
-//p => identifierCollector.Visit(p.constructorDeclaration()),
-//p => identifierCollector.Visit(p.constructorDeclaration()),
-//p => identifierCollector.Visit(p.constructorDeclaration()),
-//p => identifierCollector.Visit(p.constructorDeclaration()),
-//p => identifierCollector.Visit(p.constructorDeclaration()),
-//p => identifierCollector.Visit(p.constructorDeclaration()),
-//p => identifierCollector.Visit(p.constructorDeclaration()),
-//p => identifierCollector.Visit(p.constructorDeclaration()),
-//p => identifierCollector.Visit(p.constructorDeclaration()),
-//p => identifierCollector.Visit(p.constructorDeclaration()),
-//p => identifierCollector.Visit(p.constructorDeclaration()),
-//p => identifierCollector.Visit(p.constructorDeclaration()),
-
-			//			};
-
+			Console.ReadLine();
 		}
 	}
 }
