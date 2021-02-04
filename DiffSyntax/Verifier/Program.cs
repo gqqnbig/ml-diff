6cb5941278162b76bd21e7ad23d75f6547103b0e
QQ喵 <gqqnb2005@gmail.com>
2021-02-04 20:00:20 +0800

    Add method LabelExamplesFromGuessed()
    It reads the label file generated by createlabel.py

--- DiffSyntax/Verifier/Program.cs
+++ DiffSyntax/Verifier/Program.cs
@@ -41,48 +41,90 @@ namespace DiffSyntax
 
 			foreach (var repo in repos)
 			{
-				LabelExamples(@"D:\renaming\data\real\" + repo, @"D:\renaming\data\generated\dataset\" + repo);
+				LabelExamplesFromGuessed(repo);
 			}
 
 		}
 
-		private static void LabelExamples(string folder, string target)
+		private static void LabelExamplesFromGuessed(string repo)
 		{
-			logger.LogInformation($"Label examples from {folder}");
+			string baseFolder = @"D:\renaming\data\real";
+			logger.LogInformation($"Label examples from {repo}");
 
-			if (Directory.Exists(target))
+			string repoFolder = Path.Join(baseFolder, repo);
+			string targetFolder = Path.Join(@"D:\renaming\data\generated\dataset", repo);
+			if (Directory.Exists(targetFolder))
 			{
-				Directory.Delete(target, true);
-				logger.LogInformation($"Deleted existing target folder {target}.");
+				Directory.Delete(targetFolder, true);
+				logger.LogInformation($"Deleted existing target folder {targetFolder}.");
 			}
-			Directory.CreateDirectory(target + @"\no");
-			Directory.CreateDirectory(target + @"\yes");
+			Directory.CreateDirectory(Path.Join(targetFolder, "no"));
+			Directory.CreateDirectory(Path.Join(targetFolder, "yes"));
 
 			var diffAnalyzer = new DiffAnalyzer();
 
-			foreach (string path in System.IO.Directory.EnumerateFiles(folder, "*.diff", SearchOption.TopDirectoryOnly))
+			using (var sr = new StreamReader(Path.Join(baseFolder, repo + ".txt")))
 			{
-				try
+				string line;
+				while ((line = sr.ReadLine()) != null)
 				{
-					if (diffAnalyzer.CheckIdentifierChanges(path))
+					var parts = line.Split('|');
+					var sha = parts[0];
+					var extra = parts[1];
+					var guessedLabel = parts[2];
+
+					if (guessedLabel == "y" || guessedLabel == "skip")
 					{
-						File.Copy(path, Path.Join(target, "yes", Path.GetFileName(path)));
+						var diffPath = Path.Join(repoFolder, sha + ".diff");
+						try
+						{
+
+							if (diffAnalyzer.CheckIdentifierChanges(diffPath))
+							{
+								File.Copy(diffPath, Path.Join(targetFolder, "yes", Path.GetFileName(diffPath)));
+							}
+							else
+							{
+								File.Copy(diffPath, Path.Join(targetFolder, "no", Path.GetFileName(diffPath)));
+							}
+						}
+						catch (NotSupportedException e)
+						{
+							logger.LogWarning($"{diffPath} caused error:\n{e.Message}");
+						}
+						catch (Exception e)
+						{
+							logger.LogError(new EventId(0), e, $"{diffPath} caused error:\n{e.Message}", new object[0]);
+							throw;
+						}
 					}
-					else
-					{
-						File.Copy(path, Path.Join(target, "no", Path.GetFileName(path)));
-					}
-				}
-				catch (NotSupportedException e)
-				{
-					logger.LogWarning($"{path} caused error:\n{e.Message}");
-				}
-				catch (Exception e)
-				{
-					logger.LogError(new EventId(0), e, $"{path} caused error:\n{e.Message}", new object[0]);
-					return;
 				}
 			}
+
+
+			//foreach (string path in System.IO.Directory.EnumerateFiles(folder, "*.diff", SearchOption.TopDirectoryOnly))
+			//{
+			//	try
+			//	{
+			//		if (diffAnalyzer.CheckIdentifierChanges(path))
+			//		{
+			//			File.Copy(path, Path.Join(target, "yes", Path.GetFileName(path)));
+			//		}
+			//		else
+			//		{
+			//			File.Copy(path, Path.Join(target, "no", Path.GetFileName(path)));
+			//		}
+			//	}
+			//	catch (NotSupportedException e)
+			//	{
+			//		logger.LogWarning($"{path} caused error:\n{e.Message}");
+			//	}
+			//	catch (Exception e)
+			//	{
+			//		logger.LogError(new EventId(0), e, $"{path} caused error:\n{e.Message}", new object[0]);
+			//		return;
+			//	}
+			//}
 		}
 
 		private static void CheckExamples()
