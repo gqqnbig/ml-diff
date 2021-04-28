c0efbde0922c285ab4b67198981f954a4dde00ea
QQ喵 <gqqnb2005@gmail.com>
2021-04-28 20:55:33 +0800

    Add timeout test

--- tests/test_FilterJava.py
+++ tests/test_FilterJava.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+import pytest
+
 testFolder = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, testFolder + '/..')
 
@@ -24,3 +26,19 @@ index 0000000..8168e59
 	lines2 = FilterJava.removeNonJava(lines.strip().splitlines())
 	assert any(filter(lambda l: l.startswith('+++'), lines2)) == False
 	assert any(filter(lambda l: l.startswith('---'), lines2)) == False
+
+
+@pytest.mark.timeout(1)
+def testModeChange():
+	lines = '''
+diff --git a/ReactAndroid/src/main/java/com/facebook/react/cxxbridge/ExecutorToken.java b/ReactAndroid/src/main/java/com/facebook/react/cxxbridge/ExecutorToken.java
+new file mode 100644
+index 0000000..e69de29
+diff --git a/ReactAndroid/src/main/java/com/facebook/react/cxxbridge/JSBundleLoader.java b/ReactAndroid/src/main/java/com/facebook/react/cxxbridge/JSBundleLoader.java
+new file mode 100644
+index 0000000..09fd6a4
+--- /dev/null
++++ b/ReactAndroid/src/main/java/com/facebook/react/cxxbridge/JSBundleLoader.java
+@@ -0,0 +1,90 @@
++/**'''
+	lines2 = FilterJava.removeNonJava(lines.strip().splitlines())
